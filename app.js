b0c0d5dda719ce287abcda6fed141fdd0ec84bc0
Chris Cantu <christopher.cantu@gmail.com>
2013-05-12 00:01:06 -0500

    Initial commit of the project

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+var oneDay = 86400000;
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.use(express.compress());
+app.use(express.favicon());
+app.use(express.staticCache());
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay}));
+app.use(express.logger('dev'));
+
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
