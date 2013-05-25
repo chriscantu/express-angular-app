4e50afae9a07e9b8eaac8e26b895e2a23ad70c50
Chris Cantu <christopher.cantu@gmail.com>
2013-05-25 01:06:28 -0500

    Followed the advice of JSLint

--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -6,38 +6,37 @@ angular.module('directives', [])
 			restrict: 'E',
 			replace: true,
 			templateUrl: 'directive-templates/auth.html',
-			link: function(scope, element, attrs) {
+			link: function (scope, element, attrs) {
 
-				User.loggedIn({}, function(data) { scope.user = data }, function(data) { scope.user = data });
+				User.loggedIn({}, function (data) { scope.user = data; }, function (data) { scope.user = data; });
 				scope.auth = { invalidLogin: false, showLogin: false };
 
-				scope.authenticate = function() {
+				scope.authenticate = function () {
 					User.login({username: this.user.username, password: this.user.password},
-						function(data){
+						function (data) {
 							scope.auth = { invalidLogin: false, showLogin: false};
 							scope.user = data;
 						},
-						function(){
+						function () {
 							scope.auth = { invalidLogin : true,  showLogin : true };
-						}
-					);
+						});
 					this.user.password = '';
-				}
+				};
 
-				scope.logout = function() {
-					User.logout({}, function(){
+				scope.logout = function () {
+					User.logout({}, function () {
 						scope.user.loggedIn = false;
 					});
-				}
+				};
 
-				scope.toggleLogin = function() { scope.auth.showLogin = !scope.auth.showLogin }
+				scope.toggleLogin = function () { scope.auth.showLogin = !scope.auth.showLogin; };
 
-				scope.$watch( function() {
+				scope.$watch(function () {
 					return $location.path();
-				}, function(newValue, oldValue){
-					var links = document.querySelectorAll("div.navbar div.navbar-inner ul.nav li")
+				}, function (newValue, oldValue) {
+					var links = document.querySelectorAll("div.navbar div.navbar-inner ul.nav li");
 
-					for ( var i = 0; i < links.length - 2; i++) {
+					for (var i = 0; i < links.length - 2; i++) {
 						var href = links[i].firstElementChild;
 						var hash = href.getAttribute('href').substring(1);
 						( hash == newValue ) ? links[i].className = 'active' : links[i].className = ''
@@ -66,9 +65,7 @@ angular.module('directives', [])
                     scope.markdown = converter.makeHtml(markdown);
 
                     scope.$watch('blog.body', function(data){
-
                         scope.markdown = ( data ) ? converter.makeHtml(data) : '';
-                        console.log(scope.markdown);
                     })
                 }
             }
