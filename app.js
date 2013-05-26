5cc9b266c5abf60afd74e92926074d91099a3597
Chris Cantu <christopher.cantu@gmail.com>
2013-05-26 00:45:27 -0500

    Updated app.js to follow best practices

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+'use strict';
 
 /**
  * Module dependencies.
  */
-
 var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
@@ -15,40 +15,40 @@ var express = require('express')
 var app = express();
 var oneYear = 86400000 * 365;
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
     done(null, user._id);
 });
 
-passport.deserializeUser( function(user, done) {
-    done(null, user)
+passport.deserializeUser(function (user, done) {
+    done(null, user);
 });
 
-var authenticate = function(username, password, done) {
-  	if (username == "Chris" && password == "qwerty") {
-    		return done(null, {'_id': 'mongoid'} );
-  	};
+var authenticate = function (username, password, done) {
+    if (username === "Chris" && password === "qwerty") {
+        return done(null, {'_id': 'mongoid'});
+    }
 
-  	return done(null, false, { message: "Invalid Credentials"} );
-}
+    return done(null, false, { message: "Invalid Credentials"});
+};
 
-var authSuccess = function( req, res ) {
-	 res.send(200, {'_id': 'myid', firstName: req.body.username, loggedIn: true});
-}
+var authSuccess = function (req, res) {
+    res.send(200, {'_id': 'myid', firstName: req.body.username, loggedIn: true});
+};
 
-var isAuthenticated = function( req, res, next ) {
-    ( req.isAuthenticated() ) ? next() : res.redirect(401, '/');
-}
+var isAuthenticated = function (req, res, next) {
+    return (req.isAuthenticated()) ? next() : res.redirect(401, '/');
+};
 
-var logout = function( req, res ) {
+var logout = function (req, res) {
     req.logout();
     res.send(200, "Logged out!");
-}
+};
 
-var loggedIn = function( req, res ) {
-    ( req.isAuthenticated() ) ? authSuccess(req, res) : res.send(401, {loggedIn: false});
-}
+var loggedIn = function (req, res) {
+    return (req.isAuthenticated()) ? authSuccess(req, res) : res.send(401, {loggedIn: false});
+};
 
-passport.use( new LocalStrategy(authenticate) );
+passport.use(new LocalStrategy(authenticate));
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -58,7 +58,7 @@ app.engine('html', require('ejs').renderFile);
 
 app.use(express.favicon());
 app.use(express.compress());
-app.use(express.static( path.join(__dirname, 'public'), { maxAge: oneYear }));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: oneYear }));
 
 app.use(express.logger('dev'));
 
@@ -71,7 +71,7 @@ app.use(passport.session());
 app.use(app.router);
 
 // development only
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
@@ -79,12 +79,13 @@ app.get('/', routes.index);
 app.get('/users', isAuthenticated, user.list);
 app.get('/blog', blog.list);
 app.get('/blog/:title', isAuthenticated, blog.show);
-app.post('/blog', isAuthenticated, blog.save)
+app.post('/blog', isAuthenticated, blog.save);
 
 app.get('/loggedIn', loggedIn);
 app.post('/login', passport.authenticate('local'), authSuccess);
 app.get('/logout', logout);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function ( ){
+    console.log('Express server listening on port ' + app.get('port'));
+    return;
 });
