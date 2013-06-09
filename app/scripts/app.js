153af23c3f3fd0959897dc62e0352f3aea4b4faa
Chris Cantu <christopher.cantu@gmail.com>
2013-06-09 15:24:22 -0500

    Reworked project to use yeoman layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,10 @@
 'use strict';
 
-angular.module('expressAngularAppApp', [])
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  }]);
+angular.module('light', ['ui.bootstrap', 'services', 'directives', 'filters']).
+	config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.
+			when('/home', { controller: HomeCtrl, templateUrl: 'partials/home.html'}).
+			when('/create', { controller: CreateCtrl, templateUrl: 'partials/create.html'}).
+            when('/edit/:title', { controller: EditCtrl, templateUrl: 'partials/create.html'}).
+			otherwise({ redirectTo: "/home"});
+	}]);
